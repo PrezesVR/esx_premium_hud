5b44a7733ca8a5ba1d017a6821ecaf74a6ded275
PrezesVR <kubap216@gmail.com>
2026-01-29 05:21:41 +0100

    Create fxmanifest.lua for ESX Premium HUD
    Added fxmanifest for ESX Premium HUD with UI files.

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PrezesVR'
+description 'ESX Premium HUD'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/presets.js'
+}
+
+client_script 'client.lua'
